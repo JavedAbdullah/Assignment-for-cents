fb90ff246182b045b30a0698a810c058a0ca3828
Abdu <jave.abdullah@gmail.com>
2024-08-21 19:16:31 +0200

    created a respons for request on \calculate

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/calculate', (req, res) => {
+    res.send('ci siamo forse!')
 })
 
 app.listen(port, () => {

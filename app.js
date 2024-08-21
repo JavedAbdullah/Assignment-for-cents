b165ccb6c37effbfadc0312bb6b985750f897a53
Abdu <jave.abdullah@gmail.com>
2024-08-21 19:59:42 +0200

    created a simple server that take a POST request from client, try to create a clint, some bugs to FIX

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
+// for parsing the body of the request 
+app.use(bodyParser.json());
+
+// Endpoint POST /calculate
 app.get('/calculate', (req, res) => {
-    res.send('ci siamo forse!')
+    const { num1, num2 } = req.body;
+
+    //send data to client in JSON format
+    let sum = num1 + num2
+    res.json({
+        res: sum
+    });
 })
 
 app.listen(port, () => {

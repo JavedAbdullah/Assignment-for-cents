6dd2d4fadda1291cf1cd0a4366f56001be7550b3
Abdu <jave.abdullah@gmail.com>
2024-08-21 20:16:42 +0200

    FIX bugs: POST request on the server, solved CORS problem, and made the first app that input from client and respond

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express()
 const port = 3000
+
+//  enable CORS
+app.use(cors());
 
 // for parsing the body of the request 
 app.use(bodyParser.json());
 
 // Endpoint POST /calculate
-app.get('/calculate', (req, res) => {
+app.post('/calculate', (req, res) => {
     const { num1, num2 } = req.body;
 
     //send data to client in JSON format
-    let sum = num1 + num2
+    let sum = parseInt(num1) + parseInt(num2)
     res.json({
-        res: sum
+        result: sum
     });
 })
 

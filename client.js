4567e584a7561221392eb0410baab5abae299009
Abdu <jave.abdullah@gmail.com>
2024-08-22 15:54:05 +0200

    separate the client in another file

---
+++ client.js
@@ -0,0 +1,17 @@
+document.getElementById('dataForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+
+    const response = await fetch('http://localhost:3000/calculate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ num1, num2 })
+    });
+
+    const data = await response.json();
+    document.getElementById('result').textContent = `Server Response: ${data.result}`;
+});
